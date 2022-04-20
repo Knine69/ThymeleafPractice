bca0367f6adf3a5af23112c55f061dc259f26f07
Juan Jose Huguet <jhuguet@tsgroup.com.co>
2022-04-20 15:22:18 -0500

    ADD: Kafka message request listener

---
+++ src/main/java/com/jhuguet/ThymeleafCRUDApplication/Infrastructure/config/MessageRequest.java
@@ -0,0 +1,11 @@
+package com.jhuguet.ThymeleafCRUDApplication.Infrastructure.config;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@Data
+public class MessageRequest {
+    private String message;
+}
